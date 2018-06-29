3cd80b629f30a0d86d5f356ba353444f0b966183
Phi Huy Hoang <luumanhlapdi@gmail.com>
2018-06-30 00:34:22 +0200

    update

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask, render_template
+
+year = datetime.date.today().year
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main():
+	return render_template('index.html',year = year)
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 8000))
+    print("Starting app on port %d" % port)
+app.run(debug=False, port=port, host='0.0.0.0')
